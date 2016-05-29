ee37bd28a11fe2f183eeb3bcecea099b47c943e3
daisygrace <Daisy@Daisys-MacBook-Pro.local>
2016-05-30 11:46:22 +1200

    dropdown

--- script.js
+++ script.js
@@ -62,7 +62,25 @@
     { my_image.src = "gallery/mood_sml.gif"; }
     
     
+(function() {
 
+  "use strict";
+
+  var toggles = document.querySelectorAll(".c-hamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
 
 
     
